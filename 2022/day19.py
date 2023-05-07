fcf4be299446f255c0725c83fb8e8bb80409a325
jonathan <148167.jw@gmail.com>
2023-05-07 17:28:00 +0200

    2022 day19 model. wrong assumption line 50

--- 2022/day19.py
+++ 2022/day19.py
@@ -1,82 +1,122 @@
 from readFile import *
 
+'''
+setup of model
+each row is a blueprint:
+[[robots], [storage], [blueprints], highest_quality_level] where
+each inner-array is setup as follows:
+[ore, clay, obsidian, geode]
+blueprints example:
+1 obsidian robot costs [3, 4, 0, 0]
 
-def part1(all_blueprints):
+'''
+
+
+def part1(model):
+    for max_ore_robots in range(1, 10):
+        for max_clay_robots in range(1, 10):
+            for max_obsidian_robots in range(1, 10):
+                time = 1
+                model = reset_model(model)
+                while time <= 24:
+                    building = start_building(model, max_ore_robots, max_clay_robots, max_obsidian_robots)
+                    model = collect_ores(model)
+                    model = finish_building(model, building)
+                    time += 1
+                for row_index in range(len(model)):
+                    model[row_index][3] = max(model[row_index][3], (row_index + 1) * model[row_index][1][3])
+
     score = 0
-    for blueprint in all_blueprints:
-        ore_robot = 1
-        clay_robot = 0
-        obsidian_robot = 0
-        geode_robot = 0
-        ore = 0
-        clay = 0
-        obsidian = 0
-        geode = 0
-        time = 1
-        max_ore_robot = max(blueprint[2], blueprint[3], blueprint[5])
-        while time <= 24:
-            # If we can make a geode robot, we make a geode robot
-            building = [0, 0,]
-            if ore >= blueprint[5] and obsidian >= blueprint[6]:
-                geode_robot += 1
-                ore -= blueprint[5]
-                obsidian -= blueprint[6]
-
-            # check if we do NOT have to save ores to make a geode robot next turn
-            if ore_robot >= blueprint[5] or obsidian != blueprint[6] - obsidian_robot:
-                '''
-                Given the small timeframe we want to build:
-                as many obsidian robots as possible
-                as many clay robots as possible
-                max(blueprint[2], blueprint[3], blueprint[5]) ore robots
-                    until is max is reached, try to have clay and ore robots count as close as possible
-                '''
-
-                if ore >= blueprint[3] and clay >= blueprint[4]:
-                    obsidian_robot += 1
-                    ore -= blueprint[3]
-                    clay -= blueprint[4]
-
-                if (ore_robot == max_ore_robot or clay_robot < ore_robot) and ore > blueprint[2]:
-                    clay_robot += 1
-                    ore -= blueprint[2]
-
-                if ore_robot < max_ore_robot and clay_robot >= ore_robot and ore > blueprint[1]:
-                    ore_robot += 1
-                    ore -= blueprint[1]
-
-
-            ore += ore_robot
-            clay += clay_robot
-            obsidian += obsidian_robot
-            geode += geode_robot
-
-            time += 1
-        print(blueprint[0], ':', geode)
-        score += blueprint[0] * geode
+    print(model)
+    for row in model:
+        score += row[3]
     print(score)
     return score
 
 
+def collect_ores(model):
+    '''
+    for each row, add 1 ore to storage for each robot
+    '''
+    for row in model:
+        for i in range(4):
+            row[1][i] += row[0][i]
+    return model
+
+
+def start_building(model, max_ore_robots, max_clay_robots, max_obsidian_robots):
+    limits = [max_ore_robots, max_clay_robots, max_obsidian_robots, 9999]
+    '''
+    WRONG ASSUMPTION: first all ore robots are built, then all clay robots, 
+    then all obsidian robots, then geode robots
+    '''
+    build = [[0, 0, 0, 0] for x in range(len(model))]
+
+    for row in range(len(model)):
+        for robot_type in range(4):
+            # check if limit robots this met
+            if model[row][0][robot_type] >= limits[robot_type]:
+                continue
+
+            # check if there are enough resources to build this robot
+            enough = True
+            for ore_type in range(4):
+                if model[row][1][ore_type] < model[row][2][robot_type][ore_type]:
+                    enough = False
+                    break
+
+            if enough:
+                build[row][robot_type] += 1
+                for ore_type in range(4):
+                    model[row][1][ore_type] -= model[row][2][robot_type][ore_type]
+                break
+
+    return build
+
+
+def finish_building(model, building):
+    for row in range(len(model)):
+        for robot_type in range(4):
+            model[row][0][robot_type] += building[row][robot_type]
+    return model
+
+
+def reset_model(model):
+    for row in model:
+        row[0] = [1, 0, 0, 0]
+        row[1] = [0, 0, 0, 0]
+    return model
+
+
 def parse(f):
-    bp = []
+    model = []
     for blueprint in f:
+        row = []
+        row.append([1, 0, 0, 0])
+        row.append([0, 0, 0, 0])
         split = blueprint.split(' ')
-        blueprint_index = int(split[1].replace(':', ''))    # 0
-        ore_for_ore = int(split[6])                         # 1
-        ore_for_clay = int(split[12])                       # 2
-        ore_for_obsidian = int(split[18])                   # 3
-        clay_for_obsidian = int(split[21])                  # 4
-        ore_for_geode = int(split[27])                      # 5
-        obsidian_for_geode = int(split[30])                 # 6
-        bp.append((blueprint_index, ore_for_ore, ore_for_clay, ore_for_obsidian, clay_for_obsidian, ore_for_geode, obsidian_for_geode))
-    return bp
+        ore_for_ore = int(split[6])
+        ore_for_clay = int(split[12])
+        ore_for_obsidian = int(split[18])
+        clay_for_obsidian = int(split[21])
+        ore_for_geode = int(split[27])
+        obsidian_for_geode = int(split[30])
+        blueprint_row = []
+        blueprint_row.append([ore_for_ore, 0, 0, 0])
+        blueprint_row.append([ore_for_clay, 0, 0, 0])
+        blueprint_row.append([ore_for_obsidian, clay_for_obsidian, 0, 0])
+        blueprint_row.append([ore_for_geode, 0, obsidian_for_geode, 0])
+        row.append(blueprint_row)
+        row.append(0)
+        model.append(row)
+    return model
 
 
 if __name__ == '__main__':
     test_file = line_str(19, True)
     parsed_test_file = parse(test_file)
     assert(part1(parsed_test_file) == 33)
+    exit(0)
     file = line_str(19)
     parsed_file = parse(file)
     print('part1:', part1(parsed_file))
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # AdventOfCode
 
 All my [AdventOfCode](https://adventofcode.com/) solutions in one repository. <br>
-Remember, not all code is optimized nor the smartest implementation. The code is written to get me to the answer, and not to be optimized for production. <br>
+Remember, not all code is optimized nor the smartest implementation. The code is written to get me to the answer, and not to be optimized for production. It also can contain hard-coded variables, when allowed by the puzzle. <br>
 2021 is the first year I officially tried AoC.
 
 Table of year and language.
