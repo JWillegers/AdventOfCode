3545130685095dba6df047e1297f5471bfc3d6f4
jonathan <148167.jw@gmail.com>
2023-12-28 11:32:26 +0100

    2023 Day 13 complete

--- 2023/src/day13.py
+++ 2023/src/day13.py
@@ -2,6 +2,8 @@ def solution(input13):
     starting_index = 0
     index_next_empty_line = 0
     sumPart1 = 0
+    sumPart2 = 0
+
     while index_next_empty_line < len(input13) and starting_index < len(input13):
         #  find index_next_empty_line
         for i in range(starting_index + 1, len(input13)):
@@ -11,46 +13,34 @@ def solution(input13):
         if starting_index > index_next_empty_line:
             index_next_empty_line = len(input13)
 
-        mirror_found = False
-
         # check vertical mirror
         for col in range(len(input13[starting_index]) - 1):
-            did_break = False
+            inconsistencies_found = 0
             for row in range(starting_index, index_next_empty_line):
-                if did_break:
-                    break
                 for offset in range(min(col, len(input13[starting_index]) - 2 - col) + 1):
                     if input13[row][col - offset] != input13[row][col + offset + 1]:
-                        did_break = True
-                        break
-            if not did_break:
-                mirror_found = True
+                        inconsistencies_found += 1
+            if inconsistencies_found == 0:
                 sumPart1 += col + 1
-                break
+            elif inconsistencies_found == 1:
+                sumPart2 += col + 1
 
         # check horizontal mirror
-        if not mirror_found:
-            for row in range(starting_index, index_next_empty_line - 1):
-                did_break = False
-                for col in range(len(input13[starting_index])):
-                    if did_break:
-                        break
-                    for offset in range(min(row - starting_index, index_next_empty_line - row - 2) + 1):
-                        if input13[row - offset][col] != input13[row + offset + 1][col]:
-                            did_break = True
-                            break
-                if not did_break:
-                    mirror_found = True
-                    sumPart1 += (row - starting_index + 1) * 100
-                    break
-
-        if not mirror_found:
-            print("ERROR mirror not found")
-            exit(1)
+        for row in range(starting_index, index_next_empty_line - 1):
+            inconsistencies_found = 0
+            for col in range(len(input13[starting_index])):
+                for offset in range(min(row - starting_index, index_next_empty_line - row - 2) + 1):
+                    if input13[row - offset][col] != input13[row + offset + 1][col]:
+                        inconsistencies_found += 1
+            if inconsistencies_found == 0:
+                sumPart1 += (row - starting_index + 1) * 100
+            elif inconsistencies_found == 1:
+                sumPart2 += (row - starting_index + 1) * 100
 
         starting_index = index_next_empty_line + 1
 
     print("Part1: ", sumPart1)
+    print("Part2: ", sumPart2)
 
 
 if __name__ == '__main__':
