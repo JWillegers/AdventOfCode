b2c898fa90467f9dc7b6607abb98af68f0cd690f
jonathan <148167.jw@gmail.com>
2024-12-06 14:28:33 +0100

    2024 day 6 part 2 + documentation

--- y2024/Solutions/Day6.java
+++ y2024/Solutions/Day6.java
@@ -8,7 +8,11 @@ import java.io.IOException;
 import java.util.List;
 
 public class Day6 extends Solution {
-    List<List<CellState>> puzzleMap;
+    private List<List<CellState>> puzzleMap;
+    private Parse parse;
+    private List<String> input;
+    private int originalStartingRow;
+    private int originalStartingCol;
 
     public Day6() {
 
@@ -16,8 +20,8 @@ public class Day6 extends Solution {
 
     @Override
     public void getInput(int day, Parse parser) throws IOException {
-        List<String> input = parser.readFile(day);
-        puzzleMap = parser.make2DMapCellState(input);
+        input = parser.readFile(day);
+        this.parse = parser;
     }
 
     private int[] findInitialLocation() {
@@ -35,11 +39,15 @@ public class Day6 extends Solution {
 
     @Override
     public String partOne(){
+        puzzleMap = parse.make2DMapCellState(input);
         try {
             int[] loc = findInitialLocation();
             if (loc == null) System.exit(2406);
             int row = loc[0];
             int col = loc[1];
+            // also store globally for part2
+            originalStartingRow = loc[0];
+            originalStartingCol = loc[1];
 
             // let the guard walk around
             // will get broken by IndexOutOfBoundsException
@@ -47,6 +55,8 @@ public class Day6 extends Solution {
                 CellState direction = puzzleMap.get(row).get(col);
                 switch (direction) {
                     case CellState.UP:
+                        // if wall found, turn 90 degrees
+                        // else move 1 cell up
                         if (puzzleMap.get(row - 1).get(col) == CellState.WALL) {
                             puzzleMap.get(row).set(col, CellState.RIGHT);
                         } else {
@@ -55,6 +65,8 @@ public class Day6 extends Solution {
                             row = row - 1;
                         }
                         break;
+                    // if wall found, turn 90 degrees
+                    // else move 1 cell to the right
                     case CellState.RIGHT:
                         if (puzzleMap.get(row).get(col + 1) == CellState.WALL) {
                             puzzleMap.get(row).set(col, CellState.DOWN);
@@ -64,6 +76,8 @@ public class Day6 extends Solution {
                             col = col + 1;
                         }
                         break;
+                    // if wall found, turn 90 degrees
+                    // else move 1 cell down
                     case CellState.DOWN:
                         if (puzzleMap.get(row + 1).get(col) == CellState.WALL) {
                             puzzleMap.get(row).set(col, CellState.LEFT);
@@ -73,6 +87,8 @@ public class Day6 extends Solution {
                             row = row + 1;
                         }
                         break;
+                    // if wall found, turn 90 degrees
+                    // else move 1 cell to the left
                     case CellState.LEFT:
                         if (puzzleMap.get(row).get(col - 1) == CellState.WALL) {
                             puzzleMap.get(row).set(col, CellState.UP);
@@ -103,7 +119,101 @@ public class Day6 extends Solution {
 
     @Override
     public String partTwo() {
-        return "";
+        int loopsFound = 0;
+        // for each traveled location in part1, create a new map with one of those location replace by a wall
+        for (int i = 0; i < puzzleMap.size(); i++) {
+            for (int j = 0; j < puzzleMap.get(i).size(); j++) {
+                // don't place a wall on the starting cell
+                if (i == originalStartingRow && j == originalStartingCol) {
+                    continue;
+                }
+                CellState cell = puzzleMap.get(i).get(j);
+                // check if the cell is visited in part 1
+                if (cell != CellState.EMPTY && cell != CellState.WALL) {
+                    // create a new map (shallow copy vs deep copy)
+                    List<List<CellState>> newMap = parse.make2DMapCellState(input);
+                    newMap.get(i).set(j, CellState.WALL);
+                    if (checkNewMap(newMap))  {
+                        loopsFound++;
+                    }
+                }
+            }
+        }
+        return String.valueOf(loopsFound);
+    }
+
+    private boolean checkNewMap(List<List<CellState>> newMap) {
+        try{
+            int row = originalStartingRow;
+            int col = originalStartingCol;
+            int loopStartRow = 0;
+            int loopStartCol = 0;
+            CellState loopStartCell = CellState.WALL; // obvious wrong value in case we do visit (0, 0)
+            int steps = 0;
+            int stepLimit = newMap.size() * newMap.getFirst().size() / 2;
+            // first walk stepLimit amount of steps, as the guard doesn't always start in the loop
+            // this should give enough times for a non-loop to be filtered out
+            // but to be sure we check if we revisited the same cell in the same orientation
+            while (steps < stepLimit || !(row == loopStartRow && col == loopStartCol &&
+                    newMap.get(row).get(col) == loopStartCell)) {
+                CellState direction = newMap.get(row).get(col);
+                steps++;
+                // start of checking the loop
+                if (steps == stepLimit) {
+                    loopStartRow = row;
+                    loopStartCol = col;
+                    loopStartCell = newMap.get(row).get(col);
+                }
+
+
+                switch (direction) {
+                    // if wall found, turn 90 degrees
+                    // else move 1 cell up
+                    case CellState.UP:
+                        if (newMap.get(row - 1).get(col) == CellState.WALL) {
+                            newMap.get(row).set(col, CellState.RIGHT);
+                        } else {
+                            newMap.get(row - 1).set(col, CellState.UP);
+                            row = row - 1;
+                        }
+                        break;
+                    // if wall found, turn 90 degrees
+                    // else move 1 cell to the right
+                    case CellState.RIGHT:
+                        if (newMap.get(row).get(col + 1) == CellState.WALL) {
+                            newMap.get(row).set(col, CellState.DOWN);
+                        } else {
+                            newMap.get(row).set(col + 1, CellState.RIGHT);
+                            col = col + 1;
+                        }
+                        break;
+                    // if wall found, turn 90 degrees
+                    // else move 1 cell down
+                    case CellState.DOWN:
+                        if (newMap.get(row + 1).get(col) == CellState.WALL) {
+                            newMap.get(row).set(col, CellState.LEFT);
+                        } else {
+                            newMap.get(row + 1).set(col, CellState.DOWN);
+                            row = row + 1;
+                        }
+                        break;
+                    // if wall found, turn 90 degrees
+                    // else move 1 cell to the left
+                    case CellState.LEFT:
+                        if (newMap.get(row).get(col - 1) == CellState.WALL) {
+                            newMap.get(row).set(col, CellState.UP);
+                        } else {
+                            newMap.get(row).set(col - 1, CellState.LEFT);
+                            col = col - 1;
+                        }
+                        break;
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            // this is expected behaviour
+            return false;
+        }
+        return true;
     }
 
     @Override
