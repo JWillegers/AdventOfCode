155e071d31b75b50614b0cc2bd72196e3c9cd76c
jonathan <148167.jw@gmail.com>
2025-11-30 13:10:12 +0100

    2025 setup

---
+++ y2025/Solutions/day10.py
@@ -0,0 +1,21 @@
+from copy import deepcopy
+
+def parse() -> any:
+    ret = any
+    with open('y2025/Input/day10.txt', 'r') as file:
+        file_body = file.read()
+        if len(file_body) == 0:
+            raise Exception("File is empty")
+        ret = file_body.split('\n')
+    return ret
+
+def solvePart1(input: any) -> str:
+    return "No solution found"
+
+def solvePart2(input: any) -> str:
+    return "No solution found"
+
+if __name__ == '__main__':
+    input = parse()
+    print(solvePart1(deepcopy(input)))
+    print(solvePart2(input))
