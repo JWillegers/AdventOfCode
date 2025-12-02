fd023dd5fc6c33bb0fe8aa2c9bb5959cc2cd7d3e
jonathan <148167.jw@gmail.com>
2025-12-02 10:39:18 +0100

    day 2, playing around with regex

--- y2025/Solutions/day2.py
+++ y2025/Solutions/day2.py
@@ -1,19 +1,34 @@
 from copy import deepcopy
+import re
 
-def parse() -> any:
-    ret = any
+def parse() -> str:
+    ret = str
     with open('y2025/Input/day2.txt', 'r') as file:
         file_body = file.read()
         if len(file_body) == 0:
             raise Exception("File is empty")
         ret = file_body.split('\n')
-    return ret
+    return ret[0]
 
-def solvePart1(input: any) -> str:
-    return "No solution found"
+def solvePart1(input: str) -> str:
+    sum = 0
+    pattern = re.compile('^([0-9]+)\\1$')
+    for idpair in input.split(','):
+        lower, upper = idpair.split('-')
+        for id in range(int(lower), int(upper)+1):
+            if bool(pattern.match(str(id))):
+                sum += id
+    return str(sum)
 
 def solvePart2(input: any) -> str:
-    return "No solution found"
+    sum = 0
+    pattern = re.compile('^([0-9]+)(\\1+)$')
+    for idpair in input.split(','):
+        lower, upper = idpair.split('-')
+        for id in range(int(lower), int(upper)+1):
+            if bool(pattern.match(str(id))):
+                sum += id
+    return str(sum)
 
 if __name__ == '__main__':
     input = parse()
