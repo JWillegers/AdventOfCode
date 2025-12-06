98bacde69ad1042cf36b1533c92febaac26f951e
jonathan <148167.jw@gmail.com>
2025-12-06 11:20:09 +0100

    2025 day6

--- y2025/Solutions/day6.py
+++ y2025/Solutions/day6.py
@@ -1,21 +1,70 @@
-from copy import deepcopy
-
-def parse() -> any:
-    ret = any
+def parse() -> tuple[any, any]:
+    ret = []
+    part2 = []
     with open('y2025/Input/day6.txt', 'r') as file:
         file_body = file.read()
         if len(file_body) == 0:
             raise Exception("File is empty")
-        ret = file_body.split('\n')
-    return ret
+        part2 = file_body.split('\n')
+        for i in range(len(part2) - 1):
+            sp = part2[i].split(' ')
+            lst = []
+            for num in sp:
+                if len(num) > 0:
+                    lst.append(int(num))
+            ret.append(lst)
+        sp = part2[-1].split(' ')
+        lst = []
+        for c in sp:
+                if len(c) > 0:
+                    lst.append(c)
+        ret.append(lst)
+    return ret, part2
 
-def solvePart1(input: any) -> str:
-    return "No solution found"
+def solvePart1() -> tuple[str, any]:
+    input, inputPart2 = parse()
+    total = 0
+    for col in range(len(input[0])):
+        subtotal = 0
+        if input[-1][col] == '*':
+            subtotal = 1
+            for row in range(len(input) - 1):
+                subtotal *= input[row][col]
+        else:
+            for row in range(len(input) - 1):
+                subtotal += input[row][col]
+        total += subtotal
+
+    return str(total), inputPart2
 
 def solvePart2(input: any) -> str:
-    return "No solution found"
+    total = 0
+    nums = []
+    for col in range(len(input[0])-1, -1, -1):
+        num = ""
+        for row in range(len(input)):
+            if input[row][col] == ' ':
+                continue
+
+            if input[row][col] == '*' or input[row][col] == '+':
+                nums.append(int(num))
+                subtotal = 1 if input[row][col] == '*' else 0
+                for n in nums:
+                    if input[row][col] == '*':
+                        subtotal *= n
+                    else:
+                        subtotal += n
+                total += subtotal
+                nums.clear()
+                num = ""
+            else:
+                num += input[row][col]
+        if num != "":
+            nums.append(int(num))
+
+    return str(total)
 
 if __name__ == '__main__':
-    input = parse()
-    print('part1:', solvePart1(deepcopy(input)))
-    print('part2:', solvePart2(input))
+    answerPart1, inputPart2 = solvePart1()
+    print('part1:', answerPart1)
+    print('part2:', solvePart2(inputPart2))
