d04aa36414acc6d8421b59e7cb5c00d3603fbba2
jonathan <148167.jw@gmail.com>
2025-12-09 11:29:17 +0100

    2025 day9 part 1

--- y2025/Solutions/day9.py
+++ y2025/Solutions/day9.py
@@ -1,16 +1,26 @@
 from copy import deepcopy
 
 def parse() -> any:
-    ret = any
+    ret = []
     with open('y2025/Input/day9.txt', 'r') as file:
         file_body = file.read()
         if len(file_body) == 0:
             raise Exception("File is empty")
-        ret = file_body.split('\n')
+        lines = file_body.split('\n')
+        for line in lines:
+            s = line.split(',')
+            ret.append((int(s[0]), int(s[1])))
     return ret
 
 def solvePart1(input: any) -> str:
-    return "No solution found"
+    input = sorted(input, key=lambda x:x[0])
+    answer = 0
+    for i in range(int(len(input)/2)):
+        for j in range(len(input)-1,int(len(input)/2),-1):
+            xdif = input[j][0] - input[i][0] + 1
+            ydif = abs(input[j][1] - input[i][1]) + 1
+            answer = max(answer, xdif*ydif)
+    return str(answer)
 
 def solvePart2(input: any) -> str:
     return "No solution found"
